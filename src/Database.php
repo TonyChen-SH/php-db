9f37f811dc86059d2e80467839babedd52b80f51
chendan <chendan316@gmail.com>
2017-12-15 14:38:16 +0800

    new feature: 支持一次性执行多条sql.

--- src/Database.php
+++ src/Database.php
@@ -15,19 +15,19 @@ class Database extends \Slim\PDO\Database
 {
     /**
      * Database constructor.
-     * @param $dsn
-     * @param null $usr
-     * @param null $pwd
+     * @param       $dsn
+     * @param null  $usr
+     * @param null  $pwd
      * @param array $options
      */
-    public function __construct($dsn, $usr = null, $pwd = null, array $options = array())
+    public function __construct($dsn, $usr = null, $pwd = null, array $options = [])
     {
         parent::__construct($dsn, $usr, $pwd, $options);
     }
 
     /**
      * 获取单行数据，返回的是一个一维数组
-     * @param $sql
+     * @param      $sql
      * @param null $params
      * @return mixed
      */
@@ -41,7 +41,7 @@ class Database extends \Slim\PDO\Database
 
     /**
      * 获取数组数据, 返回的是一个二维数组
-     * @param $sql
+     * @param      $sql
      * @param null $params
      * @return mixed
      */
@@ -56,7 +56,7 @@ class Database extends \Slim\PDO\Database
     /**
      * 获取单字段数据
      * SELECT COUNT(account_name) FROM player WHERE account_name='xxxx', 只会返回字段的值：1
-     * @param $sql
+     * @param      $sql
      * @param null $params
      * @return mixed
      */
@@ -71,12 +71,19 @@ class Database extends \Slim\PDO\Database
     /**
      * 执行update/delete/insert语句
      * 只会返回执行结果为bool值
-     * @param $sql
+     * @param      $sql
+     * @param bool $isMultiSql 是否执行多条SQL语句. 比如这样的一次性执行两条:INSERT INTO `admin_role` VALUES ('1','2');
+     *                                                                        INSERT INTO `admin_role` VALUES ('xx','yy');
      * @param null $params
      * @return mixed
      */
-    public function execute($sql, $params = null)
+    public function execute($sql, $isMultiSql = false, $params = null)
     {
+        if ($isMultiSql) {
+            // 用来支持一次执行多跳SQL语句，不然会报语法错误
+            // 启用或禁用预处理语句的模拟。
+            $this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, 1);
+        }
         $statement = $this->prepare($sql);
 
         return $statement->execute($params);
